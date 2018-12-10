14d89cb107fd79d027cd3ff5df5c0484ae62ac34
Nahuel Aparicio <nahuel94@gmail.com>
2018-12-10 17:19:27 -0300

    Added Burger with Ingredients and PropTypes validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const BurgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+
+	render(){
+		//Render the ingredient
+		let ingredient = null;
+		
+		//Switch props.type to see which ingredient it is
+		switch(this.props.type){
+			case('bread-bottom'):
+				ingredient = <div className={classes.BreadBottom}></div>;
+				break;
+			case('bread-top'):
+				ingredient = (
+					<div className={classes.BreadTop}>
+						<div className={classes.Seeds1}></div>
+						<div className={classes.Seeds2}></div>
+					</div>
+				);
+				break;
+			case('meat'):
+				ingredient = <div className={classes.Meat}></div>;
+				break;
+			case('cheese'):
+				ingredient = <div className={classes.Cheese}></div>;
+				break;
+			case('salad'):
+				ingredient = <div className={classes.Salad}></div>;
+				break;
+			case('bacon'):
+				ingredient = <div className={classes.Bacon}></div>;
+				break;
+			default:
+				ingredient = null;
+		}
+		//Return the ingredient jsx
+		return ingredient;
+	}
+	
 };
+
+//PropTypes validation
+BurgerIngredient.propTypes = {
+	type: PropTypes.string.isRequired
+} 
 
 export default BurgerIngredient;
