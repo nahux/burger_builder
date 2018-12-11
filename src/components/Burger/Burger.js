6fc954797b4f7dde76e214f734d5e46ed123614a
Nahuel Aparicio <nahuel94@gmail.com>
2018-12-11 20:33:19 -0300

    Reduced the ingredients array and added message if no ingredients have been added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,13 +8,19 @@ const burger = (props) => {
 	//We receive an ingredients Object with properties as the ingredients and the value is te amount
 	//We transform that into an array of an array of BurgerIngredients 
 	//Example: [ ([(salad1),(salad2)]), ([(cheese1)]), ([(meat1)]) ]
-	const ingredientsArray = Object.keys(props.ingredients)
+	let ingredientsArray = Object.keys(props.ingredients)
 		.map( igKey => {
 				return [...Array(props.ingredients[igKey])].map((_, i) => {
 					return <BurgerIngredient key={igKey + i} type={igKey} />;
 				});
-			});
-
+			})
+		//We reduce the array to make it look: [ (salad1),(salad2), (cheese1), (meat1) ]
+		.reduce((arr,el) => {
+			return arr.concat(el)
+		},[]);
+	if(ingredientsArray.length === 0){
+		ingredientsArray = <p> Please start adding ingredients </p>;
+	}
 	return(
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top"/>
